79d3e163e126cc2c12ea15eef24529508269c358
snufka <52801250+snufka@users.noreply.github.com>
2019-10-09 18:03:26 +0200

    commit

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -15,4 +15,25 @@ setInterval(function()
 1000);
 }
   
-  startEL.addEventListener("click", TimerFu)
+startEL.addEventListener("click", TimerFu);
+
+//Display
+// first screen: displayed- #startScreen, hidden- #questions, #endscreen
+// question screen: displayed - questions[i], hidden- other questions, #startScreen, #endscreen
+// last Screen: displayed -#endScreen, hidden-#startScreen, #questions
+
+function toggleDisplay(event) {
+    var value = event.target.value;
+    if(value === "keydown") {
+      mouseEventsEl.classList.add("hide");
+      keyEventsEl.classList.remove("hide");
+    }
+    else {
+      mouseEventsEl.classList.remove("hide");
+      keyEventsEl.classList.add("hide");
+    }
+  }
+  
+  
+
+
