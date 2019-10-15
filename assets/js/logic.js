c3d7a4de0a374866f9ecc3d9a62ebeff682b7cc2
snufka <52801250+snufka@users.noreply.github.com>
2019-10-15 22:14:04 +0200

    commit

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -17,22 +17,21 @@ var highscoresFormEL = document.querySelector("#highscoresForm");
 
 
 
-var timeLeft = 75;
+var timeLeft = 10;
 var score = 0;
 
 //Activeting timer by clicking "Start Quiz"
 function TimerFu() {
-  setInterval(function () {
+  timerEL.textContent = timeLeft;
+  var timeInterval = setInterval(function () {
+    timeLeft--;
     timerEL.textContent = timeLeft;
 
-    timeLeft--;
-
-    if (timeLeft <= 0) {
-      timerEL.textContent = " "
+    if (timeLeft === 0) {
+      clearInterval(timeInterval)
     }
-  },
-    1000);
-  toggleDisplayQuestions();
+  }, 1000);
+  toggleDisplayQuestion(0);
 }
 
 startEL.addEventListener("click", TimerFu);
@@ -53,18 +52,50 @@ function toggleDisplayStartScreen() {
 
 }
 
-function toggleDisplayQuestions() {
+function toggleDisplayQuestion(index) {
 
   startScreenEL.classList.add("hide");
   questionsEL.classList.remove("hide");
   endScreenEL.classList.add("hide");
+  if (index === questions.length) {
+    toggleDisplayEndScreen();
+    return;//We are done
+  }
 
 
-  titleEL.textContent = questions[0].title;
-  choicesEL.textContent = questions[0].choices;
-  // if got answer to final question, than 
-  // toggleDisplayEndScreen()
-  slicingFu();
+  titleEL.textContent = questions[index].title;
+  document.querySelector("#choices").innerHTML = "";
+  questions[index].choices.forEach(function (entry) {
+    console.log(entry);
+    a = document.createElement('p')
+
+    a.textContent = entry;
+    a.addEventListener("click", function (event) {
+      console.log("clicked");
+
+      console.log(event);
+      console.log(event.target.innerText);
+      console.log(questions[index].answer);
+
+
+
+
+      if (event.target.innerText === questions[index].answer) {
+        score++;
+        prompt("Correct");
+
+      }
+      else {
+        timeLeft = timeLeft - 5;
+        prompt("wrong");
+      }
+      toggleDisplayQuestion(index + 1);
+    });
+
+    document.querySelector("#choices").appendChild(a);
+
+
+  })
 
 }
 
